903eb5f9f3d8a8245ea9711274e1dc36175ce218
Henric Jungheim <software@henric.org>
2020-05-31 09:18:40 -0700

    Fix compiler warning about use of possibly uninitialized pointer.

--- src/effects/VST/VSTEffect.cpp
+++ src/effects/VST/VSTEffect.cpp
@@ -3594,7 +3594,7 @@ void VSTEffect::SaveFXP(const wxFileName & fn)
 void VSTEffect::SaveFXProgram(wxMemoryBuffer & buf, int index)
 {
    wxInt32 subType;
-   void *chunkPtr;
+   void *chunkPtr = nullptr;
    int chunkSize;
    int dataSize = 48;
    char progName[28];
